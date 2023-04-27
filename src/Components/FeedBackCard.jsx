c8704cc84fd9d0649b9e01fe5a226f3cac699271
AJOYSR <jucse28.384@gmail.com>
2023-04-27 22:25:58 +0600

    adding all files

---
+++ src/Components/FeedBackCard.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const FeedBackCard = () => {
+  return (
+    <div>FeedBackCard</div>
+  )
+}
+
+export default FeedBackCard
